19bc48c3f0ed89b5bef5327e6f01695915251e18
Linval <lgriffiths@ycds.org>
2024-10-25 15:55:08 -0400

    first commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Mechanum.java
@@ -0,0 +1,326 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+import org.firstinspires.ftc.teamcode.Utilities.GearHoundsHardware;
+
+// cd /Users/linva/AppData/Local/Android/Sdk/platform-tools
+// ./adb connect 192.168.43.1:5555
+
+@TeleOp(name="Mechanum", group= "org/firstinspires/ftc/teamcode/TeleOp")
+
+/* This program is the robot's main TeleOp program which gets run
+ * constistently every TeleOperated period. This allows for driver control
+ * of the drivetrain, and operator control of all other subsytems on the robot.*/
+
+public class Mechanum extends OpMode
+{
+    // Declare the hardwareMap for the robot
+    private GearHoundsHardware robot = new GearHoundsHardware();
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+
+    private double shift = 1.0;
+
+    boolean Moving = false;
+    boolean Chaining = false;
+    @Override
+    public void init() {
+
+
+        // Intializes the hardwareMap found in "GearHoundsHardware" class
+        robot.init(hardwareMap);
+    }
+
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop() {
+        telemetry.addLine("Hello Linval!");
+        telemetry.update();
+    }
+
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+        runtime.reset();
+    }
+
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop() {
+        if (gamepad1.right_bumper) {
+            shift = 1.0;
+        }
+
+        if (gamepad1.left_bumper) {
+            shift = 0.3;
+        }
+
+        if (gamepad1.b) {
+            shift = 0.25;
+        }
+
+        if (gamepad1.a) {
+            shift = 0.5;
+        }
+
+        if (gamepad1.x) {
+            shift = 0.75;
+        }
+
+        if (gamepad1.y) {
+            shift = 1;
+        }
+
+//        if (gamepad2.left_trigger != 0) {
+//            robot.claw.setPower(gamepad2.left_trigger*.5);
+//        } else if (gamepad2.right_trigger != 0) {
+//            robot.claw.setPower(-gamepad2.right_trigger*.5);
+//        } else robot.claw.setPower(0);
+
+//        if (gamepad2.left_bumper) {
+//            robot.Servo1.setPosition(.4);
+//            robot.Servo2.setPosition(.6);
+//        } else if (gamepad2.right_bumper) {
+//            robot.Servo1.setPosition(0.6);
+//            robot.Servo2.setPosition(0.4);
+//        }
+
+//        if (-gamepad2.right_stick_y > 0) {
+//            Moving = false;
+//            robot.spinner.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//            robot.spinner.setPower(-gamepad2.right_stick_y*4);
+//        } else if (-gamepad2.right_stick_y < 0 && robot.spinner.getCurrentPosition() > 40) {
+//            Moving = false;
+//            robot.spinner.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//            robot.spinner.setPower(-gamepad2.right_stick_y*4);
+//        } else {
+//            if (Moving == false) {
+//                robot.spinner.setPower(0);
+//            }
+//        }
+//
+//        if (-gamepad2.left_stick_y > 0) {
+//            robot.lift.setPower(-gamepad2.left_stick_y);
+//        } else if (-gamepad2.left_stick_y < 0 && robot.lift.getCurrentPosition() > 40) {
+//            robot.lift.setPower(-gamepad2.left_stick_y);
+//        } else robot.lift.setPower(0);
+
+
+//        if (gamepad2.right_bumper)
+//            robot.claw.setPosition(.5);
+////
+////        if (gamepad2.left_bumper)
+////            robot.sticks.setPosition(.45);
+//
+//        if (gamepad2.left_trigger > 0.1) {
+//            Chaining = false;
+//            robot.chain.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//            robot.chain.setPower(-gamepad2.left_trigger);
+//        } else if (gamepad2.right_trigger > 0.1) {
+//            Chaining = false;
+//            robot.chain.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//            robot.chain.setPower(gamepad2.right_trigger);
+//        } else {
+//            if (Chaining == false) {
+//                robot.chain.setPower(0);
+//            }
+//        }
+//
+//        if (-gamepad2.right_stick_y > 0) {
+//            Moving = false;
+//            robot.hanger.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//            robot.hanger.setPower(-gamepad2.right_stick_y*4);
+//        } else if (-gamepad2.right_stick_y < 0 && robot.hanger.getCurrentPosition() > 40) {
+//            Moving = false;
+//            robot.hanger.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//            robot.hanger.setPower(-gamepad2.right_stick_y*4);
+//        } else {
+//            if (Moving == false) {
+//                robot.hanger.setPower(0);
+//            }
+//        }
+//
+////        if (gamepad2.left_trigger > 0.5) {
+////            robot.chain.setPower(-0.5);
+////        } else if (gamepad2.right_trigger > 0.5) {
+////            robot.chain.setPower(0.3);
+////        } else {
+////            robot.chain.setPower(0);
+////        }
+//
+//        if (gamepad2.left_stick_y < 0) {
+//            Moving = false;
+//            robot.lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//            robot.lift.setPower(gamepad2.left_stick_y);
+//        } else if (gamepad2.left_stick_y > 0 && robot.lift.getCurrentPosition() < 40) {
+//            Moving = false;
+//            robot.lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//            robot.lift.setPower(gamepad2.left_stick_y*0.85);
+//        } else {
+//            if (Moving == false) {
+//                robot.lift.setPower(0);
+//            }
+//        }
+//
+//        if (gamepad2.left_stick_button) {
+//            Moving = true;
+//            gamepad2.rumble(100);
+//        }
+//
+//        if (gamepad2.dpad_up) {
+//            robot.launcher.setPosition(1);
+//        }
+//
+//        if (gamepad2.dpad_down)
+//            robot.launcher.setPosition(-1);
+////        if () {
+////            robot.chain.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+////            robot.chain.setTargetPosition(29);
+////            robot.chain.setPower(0.5);
+////
+////    }
+//        /*if (gamepad1.right_bumper)
+//            robot.intake.setPower(1);
+//        else robot.intake.setPower(0);
+//
+//
+//        if (gamepad1.left_bumper)
+//            robot.intake.setPower(-1);
+//        else robot.intake.setPower(0);
+//*/
+
+        telemetry.addData("", "Left Front %d   Right Front %d", robot.leftFront.getCurrentPosition(), robot.rightFront.getCurrentPosition());
+
+        telemetry.addData("", "Left Back %d   Right Back %d", robot.leftBack.getCurrentPosition(), robot.rightBack.getCurrentPosition());
+
+        telemetry.addData("", "Angle  %f", robot.getAngle());
+
+        telemetry.addData("", "Angle  %f", -gamepad2.left_stick_y);
+
+//        telemetry.addData("", "chain %d", robot.chain.getCurrentPosition());
+//
+//        telemetry.addData("", "lift %d", robot.lift.getCurrentPosition());
+
+
+
+        //telemetry.addData("", "claw %d", robot.claw.getCurrentPosition());
+
+        telemetry.update();
+
+        //code taken from https://gm0.org/en/latest/docs/software/tutorials/mecanum-drive.html
+        double facing = robot.imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+        double y = -gamepad1.left_stick_y;
+        double x = gamepad1.left_stick_x;
+        double rx = gamepad1.right_stick_x;
+        if (gamepad1.options) {
+            robot.imu.resetYaw();
+        }
+
+
+//        public void drive_update() {
+//            /**gets the angle from the imu**/
+//            Orientation angles = robot.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZXY, AngleUnit.DEGREES);
+//            double angle = angles.firstAngle;
+//            /**gets squared values from the driver's stick input**/
+//            double r = Math.hypot(-gamepad1.left_stick_y, -gamepad1.left_stick_x);
+//            /**finds the desired angle that the driver wants to move the robot**/
+//            double robotAngle = Math.atan2(-gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
+//            /**sets the movement angle by finding the difference of the robots angle, the input angle and the offset value
+//             * the offset value is set by the the driver if the imu does not reset after auto*/
+//            robotAngle = robotAngle - Math.toRadians(angle) + offset;
+//            /** sets the turning value */
+//            double rightX = gamepad1.right_stick_x;
+//            /** calculates the motor powers using trig functions and previously found values */
+//            final double v1 = r * Math.cos(robotAngle) + rightX;
+//            final double v2 = r * Math.sin(robotAngle) - rightX;
+//            final double v3 = r * Math.sin(robotAngle) + rightX;
+//            final double v4 = r * Math.cos(robotAngle) - rightX;
+
+
+        double rotX = x * Math.cos(-facing) - y * Math.sin(-facing);
+        rotX = rotX * 1.1;
+        double rotY = x * Math.sin(-facing) + y * Math.cos(-facing);
+
+        double d = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+
+        double lf = (rotY + rotX + rx) / d;
+        double lb = (rotY - rotX + rx) / d;
+        double rf = (rotY - rotX - rx) / d;
+        double rb = (rotY + rotX - rx) / d;
+
+        //set rightFront negative so it goes same direction as other heels
+        robot.leftFront.setVelocity(2000 * lf * shift);
+        robot.leftBack.setVelocity(2000 * lb * shift);
+        robot.rightFront.setVelocity(2000 * rf * shift);
+        robot.rightBack.setVelocity(2000 * rb * shift);
+
+    }
+//
+//            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+//            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+//            double rx = gamepad1.right_stick_x;
+//
+//            // Denominator is the largest motor power (absolute value) or 1
+//            // This ensures all the powers maintain the same ratio,
+//            // but only if at least one is out of the range [-1, 1]
+//            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+//            double frontLeftPower = (y + x + rx) / denominator;
+//            double backLeftPower = (y - x + rx) / denominator;
+//            double frontRightPower = (y - x - rx) / denominator;
+//            double backRightPower = (y + x - rx) / denominator;
+//
+//            robot.leftFront.setPower(frontLeftPower);
+//            robot.leftBack.setPower(backLeftPower);
+//            robot.rightFront.setPower(frontRightPower);
+//            robot.rightBack.setPower(backRightPower);
+//        }
+//
+
+
+
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+
+        // set all the motors to stop
+        robot.leftFront.setPower(0);
+        robot.rightFront.setPower(0);
+        robot.leftBack.setPower(0);
+        robot.rightBack.setPower(0);
+//        robot.lift.setPower(0);
+        //robot.spinner.setPower(0);
+        //robot.claw.setPower(0);
+//        robot.claw.setPosition(0);
+    }
+
+//    // fuction used to make sure that the value inputted to the
+//    // motors stays between 1 and -1
+//    private double clamp(double x, double min, double max) {
+//
+//        return Math.max(min,Math.min(max,x));
+//
+//    }
+
+
+}
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utilities/GearHoundsHardware.java
@@ -0,0 +1,175 @@
+package org.firstinspires.ftc.teamcode.Utilities;
+
+//import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+
+// Generic robot class
+public class GearHoundsHardware extends Hardware {
+    public HardwareMap robotMap;
+
+    // Drivetrain Members
+    public DcMotorEx leftFront;
+    public DcMotorEx  rightFront;
+    public DcMotorEx  leftBack;
+    public DcMotorEx  rightBack;
+//    public HuskyLens huskyLens;
+//    public DcMotorEx hanger;
+//    public Servo launcher;
+//
+//    public Servo Servo1;
+//    public Servo Servo2;
+//    public DcMotorEx intake;
+//    public DcMotorEx lift;
+//    public Servo claw;
+//    public DcMotor chain;
+//    //public DcMotorEx spinner;
+
+    public static final int READ_PERIOD = 1;
+    //public HuskyLens huskyLens;
+
+    //public RevRoboticsCoreHexMotor intake;
+    public IMU imu;
+    public Orientation angles;
+    public Acceleration gravity;
+    //private Orientation             lastAngles = new Orientation();
+    //private double                  globalAngle;
+    private YawPitchRollAngles             lastAngles;
+    private static double                  globalAngle;
+
+    // 1000 ticks was roughly 18 in.
+    public static final double TICK_PER_INCH = 600.0/12.0;
+    public static final double MinPower = 0.3;
+
+    static final double FEET_PER_METER = 3.28084;
+
+    //Constructor
+    public GearHoundsHardware(){
+
+    }
+
+    // Override to set actual robot configuration
+    public void init(HardwareMap hwMap) {
+
+        // Save reference to Hardware map
+        robotMap = hwMap;
+
+        // Define and Initialize Motors for drivetrain
+        //huskyLens = robotMap.get(HuskyLens.class, "huskyLens");
+
+//        launcher = robotMap.get(Servo.class, "launcher");
+//        Servo1 = robotMap.get(Servo.class, "Servo1");
+//        Servo2 = robotMap.get(Servo.class, "Servo2");
+//        hanger = robotMap.get(DcMotorEx.class, "hanger");
+//        claw = robotMap.get(Servo.class, "sticks");
+//        lift = robotMap.get(DcMotorEx.class, "lift");
+//        intake = robotMap.get(DcMotorEx.class, "intake");
+        leftFront  = robotMap.get(DcMotorEx.class, "front_left");
+        rightFront = robotMap.get(DcMotorEx.class, "right_lefy");
+        leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        //spinner = robotMap.get(DcMotorEx.class, "spinner");
+//        chain = robotMap.get(DcMotor.class, "chain");
+        //spinner.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        //spinner.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        //leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBack  = robotMap.get(DcMotorEx.class, "back_left");
+        rightBack = robotMap.get(DcMotorEx.class, "right_back");
+        leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        chain.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//        chain.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//        lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        hanger.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//        hanger.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBack.setDirection(DcMotor.Direction.REVERSE);
+//        huskyLens = robotMap.get(HuskyLens.class, "huskyLens");
+
+        // Defines the REV Hub's internal IMU (Gyro)
+        imu = robotMap.get(IMU.class, "imu");
+
+        // Defines the parameters for the gyro (units)
+        IMU.Parameters imuParameters = new IMU.Parameters(
+                new RevHubOrientationOnRobot(
+                        RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                        RevHubOrientationOnRobot.UsbFacingDirection.LEFT
+                )
+        );
+        imu.initialize(imuParameters);
+        resetAngle();
+    }
+      /*  imuParameters.angleUnit           = IMU.AngleUnit.DEGREES;
+        imuParameters.accelUnit           = IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        imuParameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        imuParameters.loggingEnabled      = true;
+        imuParameters.loggingTag          = "IMU";
+        imuParameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+*/
+    // Intializes the parameters previously defined
+
+
+
+    public void resetAngle() {
+        imu.resetYaw();
+        lastAngles = imu.getRobotYawPitchRollAngles();
+        globalAngle = 0;
+    }
+
+    public double getAngle() {
+        YawPitchRollAngles angles = imu.getRobotYawPitchRollAngles();
+        double deltaAngle = angles.getYaw(AngleUnit.DEGREES) - lastAngles.getYaw(AngleUnit.DEGREES);
+        if (deltaAngle < -180)
+            deltaAngle += 360;
+        else if (deltaAngle > 180)
+            deltaAngle -= 360;
+        globalAngle += deltaAngle;
+        lastAngles = angles;
+        return globalAngle;
+    }
+//
+//    public void resetAngle()
+//    {
+//        //don't use resetAngle, it messes with the absolute heading
+//        lastAngles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZXY, AngleUnit.DEGREES);
+//
+//        globalAngle = 0;
+//    }
+//
+//    public double getAngle()
+//    {
+//        // We experimentally determined the Z axis is the axis we want to use for heading angle.
+//        // We have to process the angle because the imu works in euler angles so the Z axis is
+//        // returned as 0 to +180 or 0 to -180 rolling back to -179 or +179 when rotation passes
+//        // 180 degrees. We detect this transition and track the total cumulative angle of rotation.
+//
+//        Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZXY, AngleUnit.DEGREES);
+//
+//        double deltaAngle = angles.firstAngle - lastAngles.firstAngle;
+//
+//        if (deltaAngle < -180)
+//            deltaAngle += 360;
+//        else if (deltaAngle > 180)
+//            deltaAngle -= 360;
+//
+//        globalAngle += deltaAngle;
+//
+//        lastAngles = angles;
+//
+//        return globalAngle;
+//    }
+}
+
