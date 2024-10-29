aac1d75ce5e6dfd5f840dde0742578db6d0e4757
Linval <lgriffiths@ycds.org>
2024-10-29 19:37:10 -0400

    Linear thingy

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Mechanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Mechanum.java
@@ -106,19 +106,19 @@ public class Mechanum extends OpMode
 //            robot.Servo2.setPosition(1);
 //        }
 
-//        if (-gamepad2.right_stick_y > 0) {
-//            Moving = false;
-//            robot.linear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-//            robot.linear.setPower(-gamepad2.right_stick_y*4);
-//        } else if (-gamepad2.right_stick_y < 0 && robot.spinner.getCurrentPosition() > 40) {
-//            Moving = false;
-//            robot.linear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-//            robot.linear.setPower(-gamepad2.right_stick_y*4);
-//        } else {
-//            if (Moving == false) {
-//                robot.spinner.setPower(0);
-//            }
-//        }
+        if (-gamepad2.right_stick_y > 0) {
+            Moving = false;
+            robot.linear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.linear.setPower(-gamepad2.right_stick_y*4);
+        } else if (-gamepad2.right_stick_y < 0 && robot.linear.getCurrentPosition() > 40) {
+            Moving = false;
+            robot.linear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.linear.setPower(-gamepad2.right_stick_y*4);
+        } else {
+            if (Moving == false) {
+                robot.linear.setPower(0);
+            }
+        }
 //
 //        if (-gamepad2.left_stick_y > 0) {
 //            robot.lift.setPower(-gamepad2.left_stick_y);
