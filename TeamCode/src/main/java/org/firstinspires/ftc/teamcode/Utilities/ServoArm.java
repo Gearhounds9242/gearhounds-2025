c6c8f5ca3821a1641aaef029c689d5d57b930fea
hmock <hmock@ycds.org>
2025-02-08 10:25:12 -0500

    fixed presets

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utilities/ServoArm.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.Utilities;
+
+
+import  com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.hardware.Servo;
+@Config
+
+public class ServoArm {
+    public static double POS_OFFSET = 0;
+    private Servo servo;
+
+    public ServoArm(Servo servo) { this.servo = servo; }
+
+    public void setPosition(double pos) {servo.setPosition(POS_OFFSET + pos);}
+
+    public double getPosition() { return(servo.getPosition());}
+}
